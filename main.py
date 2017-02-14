b56af32da4f9cecb9153a159af956dcc2f66c68f
jysing <jysing87@gmail.com>
2017-02-14 09:28:34 +0100

    plot with actual data

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+import regression
 
 def main():
 	englishTot, englishA = np.loadtxt('english.txt', skiprows=0, unpack=True)
@@ -9,7 +12,24 @@ def main():
 	frenchTot = frenchTot/max(frenchTot)
 	frenchA = frenchA/max(frenchA)
 
-	print(frenchTot)
-	print(englishTot)
+	tol = 10**-6
+	alpha = 0.05
+	stoch = False
+
+	englishA = np.c_[np.ones(englishA.shape[0]), englishA]
+	w, epochs = regression.linRegression(englishA, englishTot, tol, alpha, stoch)
+	print(w, epochs)
+	line = np.dot(englishA, w)
+	print(englishA, englishTot)
+	plt.plot(englishA[:,1], line, 'g--', englishA[:,1], englishTot, 'rs')
+	plt.show()
+
+	frenchA = np.c_[np.ones(frenchA.shape[0]), frenchA]
+	w, epochs = regression.linRegression(frenchA, frenchTot, tol, alpha, stoch)
+	print(w, epochs)
+	line = np.dot(frenchA, w)
+	print(frenchA, frenchTot)
+	plt.plot(frenchA[:,1], line, 'b--', frenchA[:,1], frenchTot, 'rs')
+	plt.show()
 
 main()
