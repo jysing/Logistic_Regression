a12d66cd49f0d515e4f34999ddb60a9e33a46044
Joakim Ysing <joakim.ysing@gmail.com>
2017-02-15 15:48:01 +0100

    Read SVM formated data

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,18 @@
-from svmutil import *
+def read_svm_file(data_file_name):
+	y = []
+	x = []
+	for line in open(data_file_name):
+		line = line.split(None, 1)
+		label, features = line
+		xi = {}
+		for e in features.split():
+			ind, val = e.split(":")
+			xi[int(ind)] = float(val)
+		y += [float(label)]
+		x += [xi]
+	return (y, x)
 
 if __name__ == "__main__":
-	print("hello")
+	y, x = read_svm_file('data')
+	print(y)
+	print(x)
