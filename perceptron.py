2d88a4be5a88671a45704eb5793e9f5800fff844
Joakim Ysing <joakim.ysing@gmail.com>
2017-02-15 16:32:26 +0100

    Start of algorithm

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+# Read LIBSVM-format data.
 def read_svm_file(data_file_name):
 	y = []
 	x = []
@@ -12,7 +15,12 @@ def read_svm_file(data_file_name):
 		x += [xi]
 	return (y, x)
 
+# Perceptron learning algorithm.
+def perc_alg(x, y, w, alpha):
+	if np.dot(x, w) >= 0: 
+		hypothesis = 1
+	else :
+		hypothesis = -1
+
 if __name__ == "__main__":
 	y, x = read_svm_file('data')
-	print(y)
-	print(x)
